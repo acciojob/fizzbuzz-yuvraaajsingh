9ae16bcda392ce365c5909681ce611d9e6ef5721
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-15 13:45:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+for(let i=1;i<=20;i++)
+	{
+		if(i%3==0  && i%5==0)
+			console.log("FizzBuzz");
+		else if(i%3==0)
+			console.log("Fizz");
+		else if(i%5==0)
+			console.log("Buzz");
+	}
